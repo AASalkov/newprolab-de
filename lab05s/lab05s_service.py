28be81b71d1cd3d8943fb8c62f1507973883c1b7
Salkov Aleksandr Aleksandrovich <ASalkov@gksm.local>
2020-04-25 18:39:56 +0300

    Update lab05s files

--- lab05s/lab05s_service.py
+++ lab05s/lab05s_service.py
@@ -1,33 +1,26 @@
-import json
 from flask import Flask, request
-from joblib import load
-import pandas as pd
-import re
-from urllib.parse import urlparse
-from urllib.request import urlretrieve, unquote
+import requests
+from urllib.parse import quote
 
-def url2domain(url):
-    url = re.sub('(http(s)*://)+', 'http://', url)
-    parsed_url = urlparse(unquote(url.strip()))
-    if parsed_url.scheme not in ['http','https']: return None
-    netloc = re.search("(?:www\.)?(.*)", parsed_url.netloc).group(1)
-    #if netloc is not None: return str(netloc.encode('utf-8')).strip()
-    if netloc is not None: return str(netloc).strip()
-    return None
-
-def list2domain(rec):
-    result = []
-    for x in rec:
-       result.append(url2domain(x["url"]))
-    return ', '.join(result)
-
-print("Start")
-model = load("lab05.joblib")
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, world!'
+
+@app.route('/get_gender_age', methods=['GET'])
+def get_most_recent_stats_params():
+    ga = request.args.get('ga')
+    query = "SELECT uid from alexander_salkov_lab05s where gender_age == '{}'".format(ga.replace('"', ''))
+    url = 'http://{}:{}/?query={}'.format("85.192.33.223", "8123", quote(query))
+    response = requests.get(url)
+    res = ""
+    if response.status_code == 200:
+        # parse output
+        for rec in response.text.strip().split('\n'):
+            res = res + '{"uid": "' + rec+ '"}' + "\n"
+    print(res)
+    return res
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
